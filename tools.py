7e4347df003d1802a1d0bff997aa330cdd9ffef5
foxelas <32893423+foxelas@users.noreply.github.com>
2024-12-13 00:48:01 +0200

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,13 +1,14 @@
 import os
-import os
-
-import pillow_heif
 from PIL import Image
+from pillow_heif import register_heif_opener
+import piexif
+
+register_heif_opener()
 
 from configuration import FILE_SORTING_LIST, ACCEPTED_IMAGE_FILETYPES, VIDEO_FILETYPES
 
 
-def list_files_in_directory(target_dir, function=None, target_filetypes=ACCEPTED_IMAGE_FILETYPES + VIDEO_FILETYPES):
+def list_files_in_directory(target_dir, function=None, target_filetypes=ACCEPTED_IMAGE_FILETYPES + VIDEO_FILETYPES, **kwargs):
     file_list = []
     # Walk through the directory tree
     for root, dirnames, filenames in os.walk(target_dir):
@@ -20,7 +21,7 @@ def list_files_in_directory(target_dir, function=None, target_filetypes=ACCEPTED
 
     if function is not None:
         for full_path in file_list:
-            function(full_path)
+            function(full_path, **kwargs)
 
     return file_list
 
@@ -59,7 +60,29 @@ def convert_jpeg_to_png_bulk(target_dir):
     list_files_in_directory(target_dir, function=convert_jpeg_to_png, target_filetypes=['jpg', 'jpeg', 'JPG', 'JPEG'])
     print("Done!")
 
-def convert_heic_to_jpg(input_path, quality=95):
+
+def get_geotagging(exif):
+    geo_tagging_info = {}
+    if not exif:
+        raise ValueError("No EXIF metadata found")
+    else:
+        exif = exif.get_ifd(0x8825)
+        gps_keys = ['GPSVersionID', 'GPSLatitudeRef', 'GPSLatitude', 'GPSLongitudeRef', 'GPSLongitude',
+                    'GPSAltitudeRef', 'GPSAltitude', 'GPSTimeStamp', 'GPSSatellites', 'GPSStatus', 'GPSMeasureMode',
+                    'GPSDOP', 'GPSSpeedRef', 'GPSSpeed', 'GPSTrackRef', 'GPSTrack', 'GPSImgDirectionRef',
+                    'GPSImgDirection', 'GPSMapDatum', 'GPSDestLatitudeRef', 'GPSDestLatitude', 'GPSDestLongitudeRef',
+                    'GPSDestLongitude', 'GPSDestBearingRef', 'GPSDestBearing', 'GPSDestDistanceRef', 'GPSDestDistance',
+                    'GPSProcessingMethod', 'GPSAreaInformation', 'GPSDateStamp', 'GPSDifferential']
+
+        for k, v in exif.items():
+            try:
+                geo_tagging_info[gps_keys[k]] = str(v)
+            except IndexError:
+                pass
+        return geo_tagging_info
+
+
+def convert_heic_to_jpg(input_path, quality=95, strip_exif=False, delete_original=False):
     """
     Converts a HEIC image to JPG format with minimal quality loss.
 
@@ -69,36 +92,43 @@ def convert_heic_to_jpg(input_path, quality=95):
 
     Returns:
         None
+        :param strip_exif:
     """
     try:
         # Read the HEIC file
-        heif_file = pillow_heif.open_heif(input_path)
-
-        # Convert HEIF to a Pillow Image
-        image = Image.frombytes(
-            heif_file.mode, heif_file.size, heif_file.data, "raw", heif_file.mode, heif_file.stride
-        )
+        image = Image.open(input_path)
+        exif_dict = piexif.load(image.info["exif"])
+        exif_bytes = piexif.dump(exif_dict)
 
         output_path = os.path.splitext(input_path)[0] + '.jpg'
 
-        # Save as JPG with the specified quality
-        image.save(output_path, "JPEG", quality=quality)
+        if strip_exif:
+            exif_bytes = piexif.dump({})
 
+        # Save as JPG with the specified quality
+        image.save(output_path, "JPEG", quality=quality, exif=exif_bytes)
         print(f"Conversion successful: {output_path}")
+
+        if delete_original:
+            os.remove(input_path)
+
     except Exception as e:
         print(f"Error during conversion: {e}")
 
-def convert_heic_to_jpg_bulk(target_dir):
+def convert_heic_to_jpg_bulk(target_dir, quality=95, strip_exif=False, delete_original=False):
+
     print(f"Selected directory: {target_dir}")
     # Ensure the directory exists
     if not os.path.exists(target_dir):
         print(f"The directory '{target_dir}' does not exist.")
         return
 
-    list_files_in_directory(target_dir, function=convert_heic_to_jpg, target_filetypes=['heic', 'HEIC'])
+    list_files_in_directory(target_dir, function=convert_heic_to_jpg, target_filetypes=['heic', 'HEIC'],
+                            quality=quality, strip_exif=strip_exif, delete_original=delete_original)
     print("Done!")
-
 
 # Example usage:
 # Replace '/path/to/directory' with the actual directory path containing the images
-# convert_jpeg_to_png_bulk('G:\\My Drive\\Stickers\\μεμε')
+# import tools
+# tools.convert_jpeg_to_png_bulk('G:\\My Drive\\Stickers\\μεμε')
+# tools.convert_heic_to_jpg_bulk()
